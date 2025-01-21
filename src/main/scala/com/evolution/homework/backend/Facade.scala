ddffee10839a69a4e62dc3a30b6c6b9907b35ec0
Massimo <massimo.biancalani@gmail.com>
2025-01-21 15:00:47 +0100

    Refactoring

--- src/main/scala/com/evolution/homework/backend/Facade.scala
+++ src/main/scala/com/evolution/homework/backend/Facade.scala
@@ -61,59 +61,82 @@ object Facade {
       override def joinGame(idGenerator: GameIdGenerator, player: Player, gameType: GameType): IO[GameId] =
         for {
           id <- idGenerator.generate
-          game <- inMemoryGamesDb.find(id)
-          _ <- inMemoryPlayersDb.add(player, PlayerAsset(Tokens.zero))
+          _ <- setInitialTokens(player)
+          game <- findGame(id)
           _ <- if (game.isDefined) {
-            val currentPlayerDeals = game.get.deals
-            val newGame = game.get.copy(deals = currentPlayerDeals.updated(player, Set.empty))
-            inMemoryGamesDb.add(id, newGame)
+            addNewPlayerToTheGame(id, game.get, player)
           } else {
-            inMemoryGamesDb.add(id, Game(gameType, Map(player -> Set.empty)))
+            storeGame(id, gameType, player)
           }
         } yield (id)
+
+      private def findGame(gameId: GameId): IO[Option[Game]] = inMemoryGamesDb.find(gameId)
+
+      private def setInitialTokens(player: Player) = inMemoryPlayersDb.add(player, PlayerAsset(Tokens.zero))
+
+      private def storeGame(id: GameId, gameType: GameType, player: Player) = inMemoryGamesDb.add(id, Game(gameType, Map(player -> Set.empty)))
+      private def addNewPlayerToTheGame(id: GameId, game: Game, player: Player) = {
+        val currentPlayerDeals = game.deals
+        val newGame = game.copy(deals = currentPlayerDeals.updated(player, Set.empty))
+        inMemoryGamesDb.add(id, newGame)
+      }
 
 
       /** @return The player's cards if the game is in progress, or `None` if the game is not in progress. */
       override def getPlayerCards(gameId: GameId, player: Player): IO[Option[Set[Card]]] = for {
-        game <- inMemoryGamesDb.find(gameId)
+        game <- findGame(gameId)
         cards <- if (game.isDefined) {
-          game.get.deals.get(player) match {
-            case Some(s) if (!s.isEmpty) => IO.pure(Some(s))
-            case _ => IO.pure(None)
-          }
+          fetchCards(game.get, player)
         } else {
           IO.pure(None)
         }
       } yield (cards)
 
+      private def fetchCards(game: Game, player: Player) = game.deals.get(player) match {
+        case Some(s) if (!s.isEmpty) => IO.pure(Some(s))
+        case _ => IO.pure(None)
+      }
       /** Called to allow the implementation to do the card dealing step. You can assume that there will not be
        * multiple concurrent invocations of this method, and rely that this method will be called after
        * `joinGame` has been called for all players.
        */
       override def dealCardsForGame(dealer: Dealer, gameId: GameId): IO[Unit] = for {
-        game <- inMemoryGamesDb.find(gameId)
+        game <- findGame(gameId)
         players = game.get.deals.keySet
         newDeal <- dealer.deal(gameId, game.get.gameType.cardsPerPlayer, players)
-        newGame = game.get.copy(deals = newDeal)
-        _ <- inMemoryGamesDb.add(gameId, game = newGame)
+        _ <- updateGame(newDeal, game.get, gameId)
       } yield ()
+
+      private def updateGame(newDeal: Map[Player, Set[Card]], game: Game, gameId: GameId) = {
+        val newGame = game.copy(deals = newDeal)
+        inMemoryGamesDb.add(gameId, game = newGame)
+      }
+
 
       /** Applies a player's decision for a game in progress. If the game finishes upon this decision, the
        * implementation should apply the results to player balances.
        */
       override def makeDecision(gameId: GameId, player: Player, decision: CardGameDecision): IO[Unit] = for {
         game <- inMemoryGamesDb.find(gameId)
-        decs = game.get.decisions
-        newGame = game.get.copy(decisions = decs.updated(player, decision))
-        _ <- inMemoryGamesDb.add(gameId, newGame)
+        decs <- addNewDecisionToTheGame(gameId, game.get, player, decision)
         game <- inMemoryGamesDb.find(gameId)
-        _ <- if (decs.keySet.size >= game.get.gameType.playerPerGame - 1) {
+        _ <- if (isGameReady(decs, game.get)) {
           val gameDecisions: Map[Player, (CardGameDecision, Set[Card])] = createGameDecision(game.get)
           WinnerDecision.showDownAndResult(gameDecisions, inMemoryPlayersDb, gameRules.get(game.get.gameType).get)
         } else {
           IO()
         }
       } yield ()
+
+      private def addNewDecisionToTheGame(gameId: GameId, game: Game, player: Player, decision: CardGameDecision) = {
+        val decs = game.decisions
+        val newGame = game.copy(decisions = decs.updated(player, decision))
+        for {
+          _ <- inMemoryGamesDb.add(gameId, newGame)
+        } yield (decs)
+      }
+
+      private def isGameReady(decisions: Map[Player, CardGameDecision], game: Game)= decisions.keySet.size >= game.gameType.playerPerGame - 1
 
       private def createGameDecision(game: Game): Map[Player, (CardGameDecision, Set[Card])] =
         game.deals.map { case (player, cards) =>
--- src/main/scala/com/evolution/homework/backend/services/WinnerDecision.scala
+++ src/main/scala/com/evolution/homework/backend/services/WinnerDecision.scala
@@ -9,53 +9,45 @@ import com.evolution.homework.backend.{Card, CardGameDecision, Player}
 
 object WinnerDecision {
   def showDownAndResult(decisions: Map[Player, (CardGameDecision, Set[Card])], inMemorySimpleCrudPlayers: InMemorySimpleCrudPlayers[IO], gameRules: GameRules): IO[Unit] = {
-    val (playersWhoHaveFoldedMap,playersWhoHavePlayMap)  = decisions.partition { case (_, decision) => decision._1 == Fold}
+    val (playersWhoHaveFoldedMap, playersWhoHavePlayMap)  = decisions.partition { case (_, decision) => decision._1 == Fold}
     val (playersWhoHaveFolded,playersWhoHavePlay) = (playersWhoHaveFoldedMap.map{ case (player, dec) => player -> dec._2}, playersWhoHavePlayMap.map{ case (player, dec) => player -> dec._2})
     if (playersWhoHavePlay.isEmpty) {
-      for {
-        players <- IO.pure(playersWhoHaveFolded.keySet.toList)
-        _ <- players.traverse(pl => updateTokens(pl, -gameRules.tokensForEveryoneFolds, inMemorySimpleCrudPlayers))
-      } yield ()
+      withdrawPlayerWhoHadFolded(playersWhoHaveFolded.keySet.toList, -gameRules.tokensForEveryoneFolds, inMemorySimpleCrudPlayers)
       // everyone folded
     } else if (playersWhoHavePlay.keySet.size == 1) {
       // only one play
       for {
-        players <- IO.pure(playersWhoHaveFolded.keySet.toList)
-        _ <- players.traverse(pl => updateTokens(pl, -gameRules.tokensForOnePlayAndTheOthersFolds, inMemorySimpleCrudPlayers))
+        _ <- withdrawPlayerWhoHadFolded(playersWhoHaveFolded.keySet.toList, -gameRules.tokensForOnePlayAndTheOthersFolds, inMemorySimpleCrudPlayers)
         _ <- updateTokens(playersWhoHavePlay.keySet.head, gameRules.tokensForOnePlayAndTheOthersFolds, inMemorySimpleCrudPlayers)
       } yield ()
     } else {
 
         // at least 2 played, find the possible winner
-        val playersWithOrderedCards = playersWhoHavePlay.map { case (player, cards) => player -> cards.toList.sortBy(_.rank.intValue)}
+        val playersWithOrderedCards = playersWhoHavePlay.map { case (player, cards) => player -> cards.toList.sortBy( - _.rank.intValue)}
         val possibleWinner = whoWin(playersWithOrderedCards)
 
         possibleWinner match {
           case Some(winner) =>
             val losers = playersWhoHavePlay.filter{ case (player, _) => player != winner}.keySet.toList
             for {
-              players <- IO.pure(playersWhoHaveFolded.keySet.toList)
-              _ <- players.traverse(pl => updateTokens(pl, -gameRules.tokensForEveryoneFolds, inMemorySimpleCrudPlayers))
+              _ <- withdrawPlayerWhoHadFolded(playersWhoHaveFolded.keySet.toList, -gameRules.tokensForOnePlayAndTheOthersFolds, inMemorySimpleCrudPlayers)
               _ <- losers.traverse(pl => updateTokens(pl, -gameRules.tokensForMoreThanOnePlays, inMemorySimpleCrudPlayers))
               _ <- updateTokens(winner, gameRules.tokensForMoreThanOnePlays, inMemorySimpleCrudPlayers)
             } yield()
-          case None => for {
-            players <- IO.pure(playersWhoHaveFolded.keySet.toList)
-            _ <- players.traverse(pl => updateTokens(pl, -gameRules.tokensForEveryoneFolds, inMemorySimpleCrudPlayers))
-          } yield()
+          case None => withdrawPlayerWhoHadFolded(playersWhoHaveFolded.keySet.toList, -gameRules.tokensForOnePlayAndTheOthersFolds, inMemorySimpleCrudPlayers)
         }
     }
   }
 
   private def whoWin( players: Map[Player, List[Card]]): Option[Player] = {
-    if (players.isEmpty || players.keySet.size > 1 && players.values.head.isEmpty) None
+    if (players.isEmpty ||  moreThanOnePlayerWithAllTheCardProcessed(players)) None
     else if (players.keySet.size == 1) Some(players.keySet.head)
     else {
       val playersWithHighestCardRank = players.map { case (player, cards) => (player , cards.head)}
-      val findTheHighestCard = playersWithHighestCardRank.values.toList.sortBy(- _.rank.intValue).head
-      val newPlayers = playersWithHighestCardRank.filter { case (_, card) => card.rank.intValue == findTheHighestCard.rank.intValue}.keySet
-      val newPlayersWithCard: Map[Player, List[Card]] = newPlayers.map{ player => player -> players.get(player).get.tail }.toMap
-      whoWin(newPlayersWithCard)
+      val findTheHighestCardBetweenAllPlayers = playersWithHighestCardRank.values.toList.sortBy(- _.rank.intValue).head
+      val newPossibleWinners = playersWithHighestCardRank.filter { case (_, card) => card.rank.intValue == findTheHighestCardBetweenAllPlayers.rank.intValue}.keySet
+      val newPossibleWinnersWithoutCardAlreadyProcessed: Map[Player, List[Card]] = newPossibleWinners.map{ player => player -> players.get(player).get.tail }.toMap
+      whoWin(newPossibleWinnersWithoutCardAlreadyProcessed)
     }
   }
 
@@ -65,4 +57,11 @@ object WinnerDecision {
     currentTokes = playerAsset.tokens
     _ <- inMemorySimpleCrudPlayers.add(player, playerAsset.copy(tokens =  currentTokes.copy(amount = currentTokes.amount + value) ))
   } yield()
+
+  private def withdrawPlayerWhoHadFolded(playersWhoHaveFolded: List[Player], amount: Int, inMemorySimpleCrudPlayers: InMemorySimpleCrudPlayers[IO]): IO[Unit] =  for {
+    players <- IO.pure(playersWhoHaveFolded)
+    _ <- players.traverse(pl => updateTokens(pl, amount, inMemorySimpleCrudPlayers))
+  } yield ()
+
+  private def moreThanOnePlayerWithAllTheCardProcessed(players: Map[Player, List[Card]]): Boolean = players.keySet.size > 1 && players.values.head.isEmpty
 }
